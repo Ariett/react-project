ab32bb358ba98a49e58b75fa1650ed2f06b06ec7
Ariett <amozeva@gmail.com>
2023-12-09 10:16:08 +0200

    Remove latest

--- yachter/src/components/home/Home.jsx
+++ yachter/src/components/home/Home.jsx
@@ -1,6 +1,5 @@
-import Hero from "../hero/Hero"
-import MostLikedYachts from "../most-liked-yachts/MostLikedYachts"
-import YachtsLatest from "../yachts-latest/YachtsLatest"
+import Hero from "../hero/Hero";
+import MostLikedYachts from "../most-liked-yachts/MostLikedYachts";
 
 export default function Home() {
     const heroContent = {
@@ -13,8 +12,6 @@ export default function Home() {
             <Hero {...heroContent}/>
             
             <MostLikedYachts />
-
-            {/* <YachtsLatest /> */}
         </>
     )
 }
